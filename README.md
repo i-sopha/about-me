e1a3667cab00a81738d3e69bc8577cbf6f17b63b
Isabel Sopha <i.sopha94@gmail.com>
2023-03-08 17:50:38 -0800

    added to files for class 3 and added img

--- README.md
+++ README.md
@@ -23,37 +23,6 @@ Read the document, in its entirety, before beginning your lab.*
 
 *Below are your lab requirements in this format with the feature tasks already created.*
 
-### <u>Instructions</u>
-
-1. As a user, I would like to learn about the site owner so I can get to know them better..
-
-    - Create an About Me guessing game that utilizes HTML, CSS, & JavaScript.
-
-    - Include on your HTML page a short biography, your education history, an overview of your job experience, and any goals that you may have.
-
-    - Prompt the user a total of exactly five yes or no questions. The user input for the answer must accept either y/n or yes/no responses while taking into consideration case sensitivity by normalizing the user input so that it can be validated (hint: look into the `.toUpperCase()` or `.toLowerCase()` functions). Be sure to let the user know if they answered the question correctly by alerting them with a response.
-
-    - Add `console.log()` messages within your app to notify the user if they are correct. Before submitting, comment out (don’t delete) your `console.log()` messages and replace them with the `alerts()` to complete the project.
-
-2. As a user, I would like a personalized welcome message so that I feel like I am interacting with the site owner.
-
-    - Ask the user their name through a prompt()
-
-    - Display that name back to the user through a custom greeting welcoming them to your site.
-
-    - Display the user’s name back to them in your final message to the user.
-
-3. Using Lighthouse in the Chrome DevTools, analyze the accessibility of your application.
-
-    - The following options to generate a Lighthouse report should be selected:
-        - Mode: Navigation
-        - Device: Desktop
-        - Categories: Accessibility
-
-    - Strive for a score between 50-65. Make necessary adjustments based on the report to achieve that score.
-    
-    - Add a screenshot of your score to your README.md file.
-
 ### Developer Style Guide
 
 *A style guide is a series of standards and practices that must be followed in order for your code to be accepted by the team. This can include variable naming, file structure, and process. Below is a list of practices that you will be required to follow within this lab:*
@@ -76,5 +45,8 @@ Read the document, in its entirety, before beginning your lab.*
 
     - Follow the instructions in the “Setting Up a New Code 201 Project” document for an overview of the content that is required for your lab repo.
 
-
+- Report for Class 2:
 ![LightHouse Report](./img/lighthousescore.png)
+
+- Report for Class 3:
+![LightHouse Report](./img/lighthousescore2.png)
--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 'use strict';
 
+// CLASS 2
+let score = 0;
+
 let question1 = prompt("Yes/No: Do I like dogs?").toLowerCase();
 if (question1 === 'yes') {
   alert('I do like dogs!');
+  score++;
   // console.log(question1);
 } else if (question1 === 'no') {
   alert('Wrong. :( I love dogs! I have two.');
@@ -18,6 +22,7 @@ if (question2 === 'yes') {
   // console.log(question2);
 } else if (question2 === 'no') {
   alert("Correct! I am 5' 4''");
+  score++;
   // console.log(question2);
 } else {
   alert('You were suppose to answer y/n');
@@ -27,6 +32,7 @@ if (question2 === 'yes') {
 let question3 = prompt('Yes/No: Do I like the outdoors?').toLowerCase();
 if (question3 === 'yes') {
   alert('Yeah! I love the outdoors!');
+  score++;
   // console.log(question3);
 } else if (question3 === 'no') {
   alert('Wrong. I love the outdoors!');
@@ -39,6 +45,7 @@ if (question3 === 'yes') {
 let question4 = prompt("Yes/No: Do I like pineapple on my pizza?").toLowerCase();
 if (question4 === 'yes') {
   alert('Yes I do!! You were hoping I said no, werent you?');
+  score++;
   // console.log(question4);
 } else if (question4 === 'no') {
   alert('Incorrect. Bring me the pineapples!');
@@ -51,6 +58,7 @@ if (question4 === 'yes') {
 let question5 = prompt("Yes/No: Do I like pina coladas?").toLowerCase();
 if (question5 === 'yes') {
   alert('Yes! and getting caught in the rain.');
+  score++;
   // console.log(question5);
 } else if (question5 === 'no') {
   alert('Ehhh wrong! They\'re delicious!');
@@ -64,3 +72,83 @@ let userName = prompt('What is your name?').toUpperCase();
 console.log(userName);
 alert('Hello there, ' + userName + '!' + ' Welcome to my site all about me!');
 alert('Enjoy your time here ' + userName + '! Goodbye for now.');
+
+// // Question 6 Below
+
+let answerQ6 = '7';
+let attemptsQ6 = 0;
+
+while (attemptsQ6 < 4) {
+  let userGuessQ6 = prompt('Guess the number between 0-10');
+  attemptsQ6++;
+
+  if (userGuessQ6 === answerQ6) {
+    alert('Correct! Lucky guess!');
+    score++;
+    break;
+
+  } else if (userGuessQ6 < answerQ6) {
+    alert('Too Low!');
+
+  } else if (userGuessQ6 > answerQ6) {
+    alert('Too high!');
+
+  }
+
+  if (attemptsQ6 === 0) {
+    alert('You have 4 more attempts!!');
+  } else if (attemptsQ6 === 1) {
+    alert('You have 3 more attempts!!');
+  } else if (attemptsQ6 === 2) {
+    alert('You have 2 more attempts!!');
+  } else if (attemptsQ6 === 3) {
+    alert('This is your last attempt!');
+  }
+
+}
+
+alert('The Answer is: 7. Refresh the page to play again');
+
+// Question 7
+
+let answerQ7 = ['red', 'blue', 'green'];
+let attemptsQ7 = 0;
+
+while (attemptsQ7 < 6) {
+  let userGuessQ7 = prompt('What is my favorite color? 💡Hint: There are three possibilities!');
+  attemptsQ7++;
+
+  if (userGuessQ7 === answerQ7[0]) {
+    alert('Red is one of my favorite colors!');
+    score++;
+    break;
+
+  } else if (userGuessQ7 === answerQ7[1]) {
+    alert('Blue is one of my favorite colors!');
+    score++;
+    break;
+
+  } else if (userGuessQ7 === answerQ7[2]) {
+    alert('Green is one of my favorite colors!');
+    score++;
+    break;
+
+  } else if (userGuessQ7 === '') {
+    alert('You have to pick a color!');
+
+  } else if (userGuessQ7 !== '') {
+    alert('Wrong. Try again.');
+
+  }
+
+  alert('Possible correct answers: Red, blue, green');
+
+}
+
+// Declare 'score' variable at the top of page before question 1
+// score++; at all correct answers
+// while loop at the bottom of last question
+
+while (score <= 7) {
+  score = alert('You got ' + score + ' questions correct!');
+}
